99a49bf3cdd0b11236cc2de975176150c178234f
Vasyl Kuzurman <fasd123435@gmail.com>
2021-11-24 18:26:04 +0100

    commit

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String  domain, String query, boolean ownersOnly, int count,int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+
+}
